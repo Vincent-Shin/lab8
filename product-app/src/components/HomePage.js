ce87137ebabb096f4874c3684905f70c00793914
Vincent-Shin <maitrungtuan2002@gmail.com>
2024-03-13 19:36:31 -0600

    done exercise C

--- product-app/src/components/HomePage.js
+++ product-app/src/components/HomePage.js
@@ -38,11 +38,11 @@ const HomePage = () => {
                 </thead>
                 <tbody>
                     <tr>
-                        <td style={styles.td}>A B</td>
+                        <td style={styles.td}>Trung Tuan Mai</td>
                         <td style={styles.td}>Developer</td>
                     </tr>
                     <tr>
-                        <td style={styles.td}>C D</td>
+                        <td style={styles.td}>Zayn Dias</td>
                         <td style={styles.td}>Designer</td>
                     </tr>
                 </tbody>
